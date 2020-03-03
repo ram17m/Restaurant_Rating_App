a9232389e4fec0c64ef6cb999793e9c90fd4899e
Ram babu Mishra <ramm.metropolia.fi>
2020-03-03 14:55:15 +0200

    NativeBase styling app

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,57 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  Card,
+  CardItem,
+  Left,
+  Thumbnail,
+  Body,
+  Text,
+  Right,
+  Button
+} from "native-base";
 
 const mediaURL = "http://media.mw.metropolia.fi/wbma/uploads/";
 
 const ListItem = props => {
   const fileData = props.item;
   return (
-    <TouchableOpacity
-      onPress={() => props.navigation.push("Single", { fileData: fileData })}
-    >
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={{ uri: mediaURL + props.item.filename }}
-        />
-        <View style={styles.details}>
-          <Text style={styles.title}>{props.item.title}</Text>
-          <Text style={styles.description}>{props.item.description}</Text>
-        </View>
-      </View>
-    </TouchableOpacity>
+    <Card>
+      <CardItem>
+        <Left>
+          <Thumbnail square source={{ uri: mediaURL + props.item.filename }} />
+          <Body>
+            <Text numberOfLines={1}>{props.item.title}</Text>
+            <Text note numberOfLines={1}>
+              {props.item.description}
+            </Text>
+          </Body>
+        </Left>
+        <Right>
+          <Button
+            primary
+            onPress={() =>
+              props.navigation.push("Single", { fileData: fileData })
+            }
+          >
+            <Text>View</Text>
+          </Button>
+        </Right>
+      </CardItem>
+    </Card>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 5,
-    paddingTop: 10,
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "#f9c2ff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  details: {
-    width: "45%",
-    flex: 1,
-    flexDirection: "column",
-    padding: 10
-  },
-  image: {
-    width: "20%",
-    height: 200,
-    flex: 1,
-    flexDirection: "row",
-    margin: 10
-  },
-  title: {
-    fontWeight: "bold"
-  },
-  description: {
-    fontSize: 11
-  }
-});
 
 export default ListItem;
